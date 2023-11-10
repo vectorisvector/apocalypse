8c1135d23f5568ae827948f57dd2a6bfc943cef9
Vector <vector9527@proton.me>
2023-11-10 17:28:39 +0800

    change license

--- src/utils/oblisk.ts
+++ src/utils/oblisk.ts
@@ -1,10 +1,10 @@
-import { getMetadata, Obelisk, NetworkType } from "@0xobelisk/client";
+import { loadMetadata, Obelisk, NetworkType } from "@0xobelisk/client";
 
 const network = "devnet" as NetworkType;
 const packageId =
   "0x804578b9eed47d461bba52c393cf148302819e2ba0a0f558356cc419b3e941ed";
 
-const metadata = await getMetadata(network, packageId);
+const metadata = await loadMetadata(network, packageId);
 
 export const obelisk = new Obelisk({
   networkType: network,
