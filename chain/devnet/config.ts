1488d4b67f4e753171a841ae28d7cd2caa2af60c
Vector <vector9527@proton.me>
2023-11-18 21:20:04 +0800

    update: Update package and world IDs

--- chain/devnet/config.ts
+++ chain/devnet/config.ts
@@ -2,8 +2,8 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'devnet';
 
-const PACKAGE_ID = '0x8cf20b0eab692ac485d1dcc366248acf2e99efab9d235e210210cac8b59e341c'
-const WORLD_ID = '0x63919692163e7ac08364ca7ce21a6830856ebcf0263e1077d063ab8e3c8dcd63'
+const PACKAGE_ID = '0xe18652167da6ea4b43fccf9374b28f31491f9c60ede161452fe1af042679ea57'
+const WORLD_ID = '0xca1007676a4ff9ee4c1b24860d0eb55938e5ad29a28cc760bf29d6aa5ae7677d'
 
 export {
     NETWORK,
