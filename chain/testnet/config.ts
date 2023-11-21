f2a5f703214a3e9ba7b983436e9a4c822f4b6b89
Vector <vector9527@proton.me>
2023-11-21 18:50:58 +0800

    update: Add testnet configuration

---
+++ chain/testnet/config.ts
@@ -0,0 +1,12 @@
+type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
+
+const NETWORK: NetworkType = 'testnet';
+
+const PACKAGE_ID = '0xc46954a938c5fc9ca7b7f1d0ecce45c2aac74930f38b9f519d9190567aa8a9fd'
+const WORLD_ID = '0x86eb34bef41185a838b701bfbd6967603f09a495e1732e13bb45888be602679c'
+
+export {
+    NETWORK,
+    PACKAGE_ID,
+    WORLD_ID,
+}
